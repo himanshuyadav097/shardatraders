ecb9deec13709c32685a03a593f7b3ca2f244056
himanshu yadav <himanshu.yadav@techletsolutions.com>
2017-10-16 18:02:11 +0530

    new

--- misc/user_validation.js
+++ misc/user_validation.js
@@ -34,10 +34,30 @@ function register_user_validation()
 	}else{
 		jQuery("#address").parent().removeClass('has-error');
 	}
-		 
+	
+	if(jQuery( "#edit-phone" ).val() == "")
+	{
+		alert("Phone number is required");
+		  jQuery("#edit-phone").parent().addClass('has-error');
+	      return false;  
+	  }  
+	 else  
+	 {  
+		 jQuery("#edit-phone").parent().removeClass('has-error');
+	 }
+		var d=ValidatePhone("#edit-phone");
+				 if(d=='F')
+				 {  
+		        alert("Phone number should be and minimum 10 or less than 16 digits");  
+		        jQuery("#edit-phone").parent().addClass('has-error');
+			      return false;  
+				 }  
+				 else  
+				 {  
+							 jQuery("#edit-phone").parent().removeClass('has-error');
+				 }
+	
 }
-
-
 
 /*$("#edit-m-unit-price").blur(function () {
 
@@ -102,4 +122,49 @@ jQuery(document).ready(function(){
 			jQuery("#edit-base-price").css("border", "1px solid #ccc");
 		}
     });  
-}); 
+	
+	
+});
+
+//Not enter numeric Number
+
+jQuery(document).ready(function(){
+
+	jQuery("#edit-f-name ,#edit-l-name").keypress(function(event){
+	        var inputValue = event.which;
+	        // allow letters and whitespaces only.
+	        if(!(inputValue >= 65 && inputValue <= 120) && (inputValue != 32 && inputValue != 0)) { 
+	            event.preventDefault(); 
+	        }
+	    });  
+	});
+
+//Validate phone number function
+
+function ValidatePhone(ids) {
+	
+    var phoneRegExp = /^((\+)?[1-9]{1,2})?([-\s\.])?((\(\d{1,4}\))|\d{1,4})(([-\s\.])?[0-9]{1,12}){1,2}$/;
+    var phoneVal = jQuery(ids).val();
+    var numbers = phoneVal.split("").length;
+    if (10 <= numbers && numbers <= 16 && phoneRegExp.test(phoneVal)) {
+        return 'S';
+    }
+    else{
+    	return 'F';
+    }
+    }
+
+
+//Validate enter only integer value
+
+jQuery(document).ready(function () {
+	  //called when key is pressed in textbox
+	jQuery("#edit-qty,#edit-unit-prices,#edit-total-price,#edit-base-price,#edit-sale-price").keypress(function (event) {
+	     //if the letter is not digit then display error and don't type anything
+	     if (event.which != 8 && event.which != 0 && (event.which < 48 || event.which > 57)) {
+	        //display error message
+	    	 jQuery("#errmsg").html("Digits Only").show().fadeOut("slow");
+	               return false;
+	    }
+	   });
+	});
